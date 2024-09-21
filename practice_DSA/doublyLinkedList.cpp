043da34e82f5d7e183ae5290be3e3c9fc7ab8a01
abdullahejazjanjua <abdullahejaz822@gmail.com>
2024-09-21 16:37:14 +0500

    Added doubly Linked list

---
+++ practice_DSA/doublyLinkedList.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+class doublylist
+{
+    struct node
+    {
+        int data;
+        node *next;
+        node *prev;
+    };
+    node *headptr;
+    doublylist()
+    {
+        headptr = NULL;
+    }
+    void addAthead(int data);
+    void addAtTail(int data);
+    void display();
+    void RemoveByKey(int data);
+
+};
+
+int main()
+{
+
+}
+
+void doublylist :: addAthead(int data)
+{
+    node *newNode = new node;
+    newNode->data = data;
+    newNode->next = headptr;
+    newNode->prev = NULL;
+}
