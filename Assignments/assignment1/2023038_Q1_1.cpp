a89d7eab3224a24a26135ba0a9a62ebf0b8af85a
abdullahejazjanjua <abdullahejaz822@gmail.com>
2024-10-19 02:53:56 +0500

    Added assignment1

---
+++ Assignments/assignment1/2023038_Q1_1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+struct UniverseCoordinate
+{
+    int s_number = 0;
+    int x_position = -1;
+    int y_position = -1;
+    bool is_snake = false;
+};
+
+int main()
+{
+    
+    return 0;
+}
